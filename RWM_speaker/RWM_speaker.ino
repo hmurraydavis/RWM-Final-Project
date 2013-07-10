977c560d80fd3268fdf6897e8075a63ca38ac27d
Halie <hdavis@CTVPFV1.olin.edu>
2013-07-10 00:40:21 -0400

    first commit

---
+++ RWM_speaker/RWM_speaker.ino
@@ -0,0 +1,24 @@
+/*
+Code to make a speaker sound. This will be used as an indicator. 
+
+Written by Halie Murray-Davis for Real World Measurements. 3/29/2013.
+*/
+
+#include "pitches.h"
+#include "SoftwareSerial.h"
+
+int leds []= {0, 1, 2, 3, 4}; //set pins for LEDS
+int melody []= {NOTE_A3, NOTE_C4, NOTE_E4, NOTE_A5, NOTE_F4};
+int rhythm[] = {2, 1, 1, 4, 8};
+int fakesensor = 0;
+
+void setup() {
+for (int thisNote=0; thisNote <5; thisNote++) {
+    int cay=5;
+    }
+}
+
+void loop() {
+int fish=6;
+}
+
