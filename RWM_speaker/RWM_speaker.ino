d37c0fd1073604f7ba716a2bf2f2a919056d7ad4
Halie <hmurraydavis@yahoo.com>
2013-07-13 21:52:29 -0400

    Version 2
    Second version of the code.

--- RWM_speaker/RWM_speaker.ino
+++ RWM_speaker/RWM_speaker.ino
@@ -10,14 +10,36 @@ Written by Halie Murray-Davis for Real World Measurements. 3/29/2013.
 int leds []= {0, 1, 2, 3, 4}; //set pins for LEDS
 int melody []= {NOTE_A3, NOTE_C4, NOTE_E4, NOTE_A5, NOTE_F4};
 int rhythm[] = {2, 1, 1, 4, 8};
-int fakesensor = 0;
+int fakesensor = A0;
 
 void setup() {
-for (int thisNote=0; thisNote <5; thisNote++) {
-    int cay=5;
-    }
+    pinMode(fakesensor, INPUT);
+    for (int x=0; x<5; x++) {
+      pinMode(leds [x], OUTPUT);
+      leds[x]=LOW;
+    }    
+    Serial.begin(4800); 
 }
 
 void loop() {
-int fish=6;
+  //Serial.print("Hello world.");totestserialport
+  int val = analogRead(fakesensor);    // read the input pin
+  Serial.println(val);             // debug value
+    if (val>=680){
+        for (int thisNote=0; thisNote <5; thisNote++) {
+          int rhythms=1000/rhythm[thisNote];
+          tone(8, melody[thisNote], rhythms);
+          int pause=rhythms * 1.3;
+          delay(pause);
+          noTone(8);
+        }
+         for (int y=0; y<4; y++) {
+           digitalWrite (leds [y], HIGH);
+         }
+        
+    }
+    
+   
 }
+
+
